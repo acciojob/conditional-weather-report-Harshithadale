1f5ec4ddffcd2c76a21440e62ce4a7d00877da68
harshithadale <daleharshi6045@gmail.com>
2025-04-16 09:01:33 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,31 @@
-import React, {useEffect, useState } from 'react'
-import '../styles/App.css'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+import axios from "axios";
 
 function App() {
-  const [WeatherDisplay,setWeatherDisplay ]= useState(true)
-  const staticWeatherInp = { temperature: 25, conditions: "Sunny" }
-  useEffect(()=>{
-    if(staticWeatherInp.temperature > 20){
-    setWeatherDisplay(false)
-      }
+  const [WeatherDisplay, setWeatherDisplay] = useState(true);
+  const staticWeatherInp = { temperature: 25, conditions: "Sunny" };
+  useEffect(() => {
+    if (staticWeatherInp.temperature > 20) {
+      setWeatherDisplay(false);
     }
-      ,[])
+  }, []);
   return (
     <div>
-        {WeatherDisplay ? <p style={{color:"blue"}}>{staticWeatherInp.temperature}</p> : <p style={{color:"red"}}>{staticWeatherInp.temperature}</p>}
-        <p>{staticWeatherInp.conditions}</p>
+      {WeatherDisplay ? (
+        <p>
+          Temperature:{" "}
+           <span style={{ color: "blue" }}>{staticWeatherInp.temperature}</span>
+        </p>
+      ) : (
+        <p>
+          Temperature:{" "}
+           <span style={{ color: "red" }}> {staticWeatherInp.temperature}</span>
+        </p>
+      )}
+      <p>Conditions: {staticWeatherInp.conditions}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
